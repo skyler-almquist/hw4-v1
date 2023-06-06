0e3df90e588b18525ed633fd9cb50a74612412fa
Skyler Almquist <skyler.almquist@kellogg.northwestern.edu>
2023-06-06 04:16:25 +0000

    we are getting there - able to log in and have pages link together

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,27 @@
 class SessionsController < ApplicationController
   def new
   end
-
+  
   def create
+    @user = User.find_by({ "username" => params["username"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Sup Homie."
+        redirect_to "/places"
+      else
+        flash["notice"] = "You Dumbass."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "You Dumbass."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Peace Out Dawg."
+    redirect_to "/login"
   end
 end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources "posts"
   resources "sessions"
   resources "users"
+
+   # Login/out
+   get "/login", :controller => "sessions", :action => "new"
+   get "/logout", :controller => "sessions", :action => "destroy"
+ 
+   # Define the root route
+   get "/", :controller => "marketing_pages", :action => "home"
+   
 end
